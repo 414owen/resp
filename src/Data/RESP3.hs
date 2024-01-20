74bb2ae0248d7c2408bd22e6a44cd107b8dcfe7a
Owen Shepherd <owen@owen.cafe>
2024-01-20 18:59:03 +0100

    feat: More protocol implementation, more tests

--- src/Data/RESP3.hs
+++ src/Data/RESP3.hs
@@ -6,74 +6,116 @@ module Data.RESP3
   , reply
   ) where
 
-import qualified Data.ByteString       as BS
-import qualified Data.ByteString.Lazy  as BSL
 import qualified Data.ByteString.Char8 as BS8
 import qualified Data.Text.Encoding    as Text
 import qualified Scanner               as Scanner
 
-import Data.ByteString (ByteString)
-import Data.Char       (digitToInt)
-import Data.Int        (Int64)
-import Data.Text       (Text)
-import Scanner         (Scanner)
-import Control.Monad (when)
+import Data.ByteString      (ByteString)
+import Data.Char            (digitToInt)
+import Data.Int             (Int64)
+import Data.Text            (Text)
+import Scanner              (Scanner)
+import Control.Monad        (when, replicateM)
+import Data.ByteString.Lazy (LazyByteString)
+import qualified Data.ByteString.Lazy as BSL
+import Data.Functor (($>))
 
 data RespReply
   = RespString Text
   | RespBlob ByteString
-  | RespStreamingBlob BSL.ByteString
-  -- TODO will this always be text?
+  | RespStreamingBlob LazyByteString
+
+  -- The redis convention of including an error code prefix
+  -- in the form "-PREFIX error message\r\n"
+  -- is just that, a convention, and isn't part of the
+  -- RESP protocol.
   | RespStringError Text
-  | RespBinaryError ByteString
+
+  -- TODO
+  | RespBlobError ByteString
+
+  | RespArray [RespReply]
   | RespInteger Int64
+  | RespNull
+  | RespBool Bool
   deriving (Show, Eq)
 
 data MessageSize
   = MSVariable
+  | MSMinusOne
   | MSFixed Int
 
 reply :: Scanner RespReply
-reply = do
-  c <- Scanner.anyChar8
-  case c of
-    '$' -> scanBlob
-    '+' -> scanString
-    '-' -> scanError
-    ':' -> scanInteger
-    _ -> fail "Unknown reply type"
+reply = Scanner.anyChar8 >>= scanTopLevelReply
 
-scanMessageSize :: Scanner MessageSize
-scanMessageSize = do
-  line <- scanLine
-  case line of
-    "?" -> pure MSVariable
-    _ -> pure $ MSFixed $ parseNatural line
+scanTopLevelReply :: Char -> Scanner RespReply
+scanTopLevelReply c = case c of
+  '$' -> scanBlob
+  '+' -> scanString
+  '-' -> scanStringError
+  ':' -> scanInteger
+  '*' -> scanArray
+  '_' -> scanEol $> RespNull
+  '#' -> RespBool . (== 't') <$> Scanner.anyChar8 <* scanEol
+  _ -> fail "Unknown reply type"
 
 parseNatural :: Integral a => ByteString -> a
 parseNatural = BS8.foldl' (\a b -> a * 10 + fromIntegral (digitToInt b)) 0
 
-scanBlob :: Scanner RespReply
-scanBlob = RespBlob <$> do
-  ms <- scanMessageSize
-  case ms of
-    MSFixed n -> Scanner.take n <* scanEol
-    MSVariable -> streamingBlobParts
+-- RESP2 calls these 'multi bulk'
+-- RESP3 calls it an 'array'
+scanArray :: Scanner RespReply
+scanArray = do
+  messageSize <- scanComplexMessageSize
+  case messageSize of
+    MSFixed n -> RespArray <$> replicateM n reply
+    MSMinusOne -> pure RespNull
+    MSVariable -> RespArray <$> scanVarArrayItems
 
-streamingBlobParts :: Scanner ByteString
+-- See https://github.com/redis/redis-specifications/blob/master/protocol/RESP3.md#streamed-aggregated-data-types
+scanVarArrayItems :: Scanner [RespReply]
+scanVarArrayItems = do
+  c <- Scanner.anyChar8
+  case c of
+    '.' -> scanEol $> []
+    _ -> (:) <$> scanTopLevelReply c <*> scanVarArrayItems
+
+-- RESP2 calls these 'bulk strings'
+-- RESP3 calls them 'blob strings' (in the markdown, on the website they're still 'bulk strings')
+scanBlob :: Scanner RespReply
+scanBlob = do
+  ms <- scanComplexMessageSize
+  case ms of
+    MSFixed n -> RespBlob <$> Scanner.take n <* scanEol
+    MSVariable -> RespStreamingBlob . BSL.fromChunks <$> streamingBlobParts
+    MSMinusOne -> pure RespNull
+
+scanMessageSize :: Scanner Int
+scanMessageSize = parseNatural <$> scanLine
+
+-- Used for blobs and arrays
+scanComplexMessageSize :: Scanner MessageSize
+scanComplexMessageSize = do
+  line <- scanLine
+  case line of
+    "?" -> pure MSVariable
+    "-1" -> pure MSMinusOne
+    _ -> pure $ MSFixed $ parseNatural line
+
+streamingBlobParts :: Scanner [ByteString]
 streamingBlobParts = do
   expectChar ';'
-  undefined
+  ms <- scanMessageSize
+  case ms of
+    0 -> pure mempty
+    n -> (:) <$> Scanner.take n <* scanEol <*> streamingBlobParts
 
--- TODO check RESP3 spec
 scanString :: Scanner RespReply
 scanString = RespString . Text.decodeUtf8 <$> scanLine
 
--- TODO check RESP3 spec
-scanError :: Scanner RespReply
-scanError = RespStringError . Text.decodeUtf8 <$> scanLine
+scanStringError :: Scanner RespReply
+scanStringError = RespStringError . Text.decodeUtf8 <$> scanLine
 
--- TODO check RESP3 spec
 scanInteger :: Scanner RespReply
 scanInteger = RespInteger . parseNatural <$> scanLine
 
--- test/Main.hs
+++ test/Main.hs
@@ -8,18 +8,106 @@ import Data.Text        (Text)
 import Scanner
 import Test.Tasty
 import Test.Tasty.HUnit
+import Data.ByteString.Lazy (LazyByteString)
+import Data.Int (Int64)
 
-testStrSucceeds :: ByteString -> Text -> Assertion
-testStrSucceeds bs expected = scanOnly reply bs @?= Right (RespString expected)
+scanReply :: ByteString -> Either String RespReply
+scanReply = scanOnly reply
 
-testBlobSucceeds :: ByteString -> ByteString -> Assertion
-testBlobSucceeds bs expected = scanOnly reply bs @?= Right (RespBlob expected)
+testStr :: ByteString -> Text -> Assertion
+testStr bs expected = scanReply bs @?= Right (RespString expected)
+
+testBlob :: ByteString -> ByteString -> Assertion
+testBlob bs expected = scanReply bs @?= Right (RespBlob expected)
+
+testStreamingBlob :: ByteString -> LazyByteString -> Assertion
+testStreamingBlob bs expected = scanReply bs @?= Right (RespStreamingBlob expected)
+
+testInt :: ByteString -> Int64 -> Assertion
+testInt bs expected = scanReply bs @?= Right (RespInteger expected)
+
+testArray :: ByteString -> [RespReply] -> Assertion
+testArray bs expected = scanReply bs @?= Right (RespArray expected)
 
 main :: IO ()
 main = defaultMain $ testGroup "Tests"
-  [ testCase "Simple String" $ testStrSucceeds "+Test me\r\n" "Test me"
-  , testGroup "Blobs"
-    [ testCase "Simple" $ testBlobSucceeds "$7\r\nTest me\r\n" "Test me"
-    , testCase "Multiline" $ testBlobSucceeds "$15\r\nTest me\r\nline 2\r\n" "Test me\r\nline 2"
+  [ testGroup "simple string"
+    [ testCase "empty string" $ testStr "+\r\n" ""
+    , testCase "nonempty string" $ testStr "+test me\r\n" "test me"
     ]
+
+  , testGroup "simple blobs"
+    [ testCase "empty" $ testBlob "$0\r\n\r\n" ""
+    , testCase "simple" $ testBlob "$7\r\ntest me\r\n" "test me"
+    , testCase "multiline" $ testBlob "$15\r\ntest me\r\nline 2\r\n" "test me\r\nline 2"
+    , testCase "unicode" $ testBlob "$11\r\n( ͡° ͜ʖ ͡°)\r\n" "( ͡° ͜ʖ ͡°)"
+    , testCase "not enough bytes" $ scanReply "$10\r\nhello\r\n" @?= Left "No more input"
+    , testCase "too many bytes" $ scanReply "$2\r\nhello\r\n" @?= Left "Expected '\\r', but got 'l'"
+    ]
+
+  , testGroup "streaming blob parts"
+    [ testCase "empty" $ testStreamingBlob "$?\r\n;0\r\n\r\n" ""
+    , testCase "one-part" $ testStreamingBlob "$?\r\n;3\r\nwow\r\n;0\r\n" "wow"
+    , testCase "two-part" $ testStreamingBlob "$?\r\n;4\r\nhell\r\n;7\r\no world\r\n;0\r\n" "hello world"
+    , testCase "three-part" $ testStreamingBlob "$?\r\n;4\r\nhell\r\n;3\r\no w\r\n;4\r\norld\r\n;0\r\n" "hello world"
+    ]
+
+  , testGroup "integer"
+    -- We currently parse a zero-digit integer as 0,
+    -- even though it's technically an invalid response
+    -- in the spec. Sometimes being lenient is efficient.
+    [ testCase "empty" $ testInt ":\r\n" 0
+    , testCase "zero" $ testInt ":0\r\n" 0
+    , testCase "one" $ testInt ":1\r\n" 1
+    , testCase "forty-two" $ testInt ":42\r\n" 42
+    ]
+
+  , testGroup "null"
+    [ testCase "RESP2 bulk string null" $ scanReply "$-1\r\n" @?= Right RespNull
+    ]
+
+  , testGroup "array"
+    [ testCase "empty" $ scanReply "*0\r\n" @?= Right (RespArray [])
+    , testCase "[hello, world]"
+      $ testArray
+        "*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n"
+        [RespBlob "hello", RespBlob "world"]
+    , testCase "[1 .. 3]"
+      $ testArray
+        "*3\r\n:1\r\n:2\r\n:3\r\n"
+        $ RespInteger <$> [1..3]
+    , testCase "heterogeneous"
+      $ testArray
+        "*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$5\r\nhello\r\n"
+        $ map RespInteger [1..4] <> [RespBlob "hello"]
+
+    , testCase "nested"
+      $ testArray
+        "*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Hello\r\n-World\r\n"
+        $ RespArray <$> [RespInteger <$> [1..3], [RespString "Hello", RespStringError "World"]]
+
+    -- website: "Null arrays"
+    , testCase "null" $ scanReply "*-1\r\n" @?= Right RespNull
+
+    -- website: "Null elements in arrays"
+    , testCase "null element"
+      $ testArray
+        "*3\r\n$5\r\nhello\r\n$-1\r\n$5\r\nworld\r\n"
+        [RespBlob "hello", RespNull, RespBlob "world"]
+
+    -- from markdown spec
+    , testCase "streaming"
+      $ testArray
+        "*?\r\n:1\r\n:2\r\n:3\r\n.\r\n"
+        $ RespInteger <$> [1..3]
+
+    ]
+
+    , testCase "null" $ scanReply "_\r\n" @?= Right RespNull
+
+    , testGroup "booleans"
+      [ testCase "true" $ scanReply "#t\r\n" @?= Right (RespBool True)
+      , testCase "false" $ scanReply "#f\r\n" @?= Right (RespBool False)
+      ]
+
   ]
